7e5812b2ec803bffb7bd6f68d4463aa7897f7104
Miguel Acosta <ma.acostaw@uniandes.edu.co>
2020-10-10 11:11:25 -0500

    Final changes

--- scripts/ts/dataCourses.js
+++ scripts/ts/dataCourses.js
@@ -1,9 +1,9 @@
 import { Course } from './course.js';
 export var dataCourses = [
-    new Course("Ingeniería de Sw", "Pablo Picasso", 4),
-    new Course("Futbol 1", "Freddy Rincón", 2),
-    new Course("Algoritmos", "Carlos Fuentes", 2),
-    new Course("Estructuras de Datos", "Yesid D", 1),
-    new Course("Futbol 2", "James R", 6),
-    new Course("Jardín Zen", "Edgar", 1)
+    new Course("Desarrollo de SW", "José Bocanegra", 6),
+    new Course("Futbol 1", "Freddy Rincón", 1),
+    new Course("Dalgo", "Rodrigo Cardozo", 3),
+    new Course("Arquiemp", "Maria Romero", 3),
+    new Course("Futbol 2", "James R", 1),
+    new Course("Jardín Zen", "Edgar", 2)
 ];
--- ts/dataCourses.ts
+++ ts/dataCourses.ts
@@ -1,10 +1,10 @@
 import { Course } from './course.js';
 
 export const dataCourses = [
-  new Course("Ingeniería de Sw", "Pablo Picasso", 4),
-  new Course("Futbol 1", "Freddy Rincón", 2),
-  new Course("Algoritmos", "Carlos Fuentes", 2),
-  new Course("Estructuras de Datos", "Yesid D", 1),
-  new Course("Futbol 2", "James R", 6),
-  new Course("Jardín Zen", "Edgar", 1)
+  new Course("Desarrollo de SW", "José Bocanegra", 6),
+  new Course("Futbol 1", "Freddy Rincón", 1),
+  new Course("Dalgo", "Rodrigo Cardozo", 3),
+  new Course("Arquiemp", "Maria Romero", 3),
+  new Course("Futbol 2", "James R", 1),
+  new Course("Jardín Zen", "Edgar", 2)
 ]
